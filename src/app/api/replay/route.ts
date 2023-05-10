9174a0bd922446580d7f0c499e53cb56146a0095
Gary Storey <garystorey655@gmail.com>
2023-05-10 17:54:53 -0400

    update route

--- src/app/api/replay/route.ts
+++ src/app/api/replay/route.ts
@@ -1,4 +1,5 @@
 import { connect } from "@planetscale/database"
+import { NextRequest, NextResponse } from "next/server"
 
 const config = {
   host: process.env.DATABASE_HOST,
@@ -8,10 +9,10 @@ const config = {
 
 const conn = connect(config)
 
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
   const results = await conn.execute("select * from replay limit 10")
-  return {
+  return NextResponse.json({
     status: "ok",
     data: results.rows,
-  }
+  })
 }
