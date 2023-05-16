4466d203a06e3aff0454c8b9c5da1d94966e1a0d
Gary Storey <garystorey655@gmail.com>
2023-05-16 16:57:55 -0400

    update replay route

--- src/app/api/replay/route.ts
+++ src/app/api/replay/route.ts
@@ -12,11 +12,14 @@ const conn = connect(config)
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
   const id = searchParams.get("id")
-  const results = await conn.execute("select * from Replay where id=:id", { id })
-  console.log(results.rows)
+  const sql = !!id
+    ? "select * from Replay where id=:id"
+    : "select * from Replay order by created desc limit 10"
+  const results = await conn.execute(sql, { id })
+  const success = results.rows.length !== 0
   return NextResponse.json({
-    status: "ok",
-    data: results.rows,
+    status: success ? "ok" : "error",
+    data: success ? results.rows : [],
   })
 }
 
@@ -24,18 +27,14 @@ export async function POST(request: NextRequest) {
   try {
     const body = await request.json()
     const data = JSON.stringify({ data: body.data })
-    const sql = `
-  insert into Replay 
-    (id,name,description,characterId,controllerId,data,updated) 
-  values 
-    (?,"Untitled Entry","No description","clhi9mo3a0006apygpl9xoeyp","clhi9poel000aapygxofo3uaf",?,?)
-`
-    const results = await conn.execute(sql, [createId(), data, new Date()])
-    console.log(results)
+    const id = createId()
+    const sql = `insert into Replay (id,name,description,characterId,controllerId,data,updated) values (?,"Untitled Entry","No description","clhi9mo3a0006apygpl9xoeyp","clhi9poel000aapygxofo3uaf",?,?)`
+    const results = await conn.execute(sql, [id, data, new Date()])
+    const success = results.rowsAffected === 1
     return NextResponse.json({
-      status: 200,
-      statusText: "ok",
-      id: results.insertId,
+      status: success ? 200 : 500,
+      statusText: success ? "ok" : "error",
+      id: success ? id : "",
     })
   } catch (err) {
     console.log(err)
